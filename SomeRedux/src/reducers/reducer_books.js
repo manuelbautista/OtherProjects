d1dc30d5bf6a55eae9359acaa90c8a10a48d3480
Manuel Bautista <b0hcoder@gmail.com>
2019-02-28 19:42:39 -0400

    Some Redux

---
+++ SomeRedux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript the good part', pages: 101 },
+        {title: 'Harry poter', pages: 39},
+        {title: 'The dark towel', pages: 85}, 
+        {title: 'Eloquent Ruby', pages: 1},
+    ]
+}
